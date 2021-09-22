2ecdd20115ee410f37e3b137268de7016a47208b
Fazel Saeedi <fazeltsaeedi@yahoo.com>
2021-09-22 11:35:45 +0330

    nginx & 2 application initialize on Docker

---
+++ dotnetdocker2/WeatherForecast.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace dotnetdocker2
+{
+    public class WeatherForecast
+    {
+        public String ApplicationName { get; set; }
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
